69439a48b7e17ab999546e43ac50e02e7a0c1814
GiraMorales <gira.morales@gamil.com>
2025-01-14 21:31:26 +0100

    aceptar cookies

--- Back/index.js
+++ Back/index.js
@@ -4,6 +4,10 @@ const scrapper = async (url) => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
+  await page.$eval(
+    'button.jad_cmp_paywall_button.jad_cmp_paywall_button-cookies ',
+    (el) => el.click()
+  );
 };
 
 scrapper('https://www.sensacine.com/peliculas/mejores-peliculas/');
